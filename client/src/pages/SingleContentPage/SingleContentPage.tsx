ffba27cf0474fd18b978f9c5f7e6e939999dd304
Liat Pardo Grinbaum <102147870+LiatPardoGrinbaum@users.noreply.github.com>
2023-02-19 23:53:34 +0200

    added single content page image and set some css

--- client/src/pages/SingleContentPage/SingleContentPage.tsx
+++ client/src/pages/SingleContentPage/SingleContentPage.tsx
@@ -5,7 +5,7 @@ import { ApiResponse, useFetch } from "../../hooks/useFetch";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import "./SingleContentPage.css";
 import { Box } from "@mui/material";
-import { original } from "../../config/config";
+import { img_500, original, unavailable } from "../../config/config";
 
 type ParamId = {
   id: string;
@@ -18,14 +18,20 @@ type singleContentProps = {
 const SingleContentPage = (props: singleContentProps) => {
   const { id } = useParams<ParamId>();
 
-  const { responseData, error }: ApiResponse = useFetch(
+  const { responseData, error, loading }: ApiResponse = useFetch(
     `https://api.themoviedb.org/3/${props.media_type}/${id}?api_key=${process.env.REACT_APP_OMDB_API_KEY}&language=en-US`
   );
+  //fetch movie trailer video:
   const { results }: ApiResponse = useFetch(
-    `https://api.themoviedb.org/3/${props.media_type}/${id}/videos?api_key=08a1dc0b34c256c4888328ae48e28713&language=en-US`
+    `https://api.themoviedb.org/3/${props.media_type}/${id}/videos?api_key=${process.env.REACT_APP_OMDB_API_KEY}&language=en-US`
   );
   console.log("responseData", responseData);
+  console.log("results", results);
   console.log("err", error);
+
+  if (loading) {
+    return <div>Loading</div>;
+  }
 
   return (
     <Box
@@ -35,10 +41,25 @@ const SingleContentPage = (props: singleContentProps) => {
       alignItems="center"
       width="100%"
       className="singleContentPage">
+      {responseData && (
+        <img
+          src={responseData.backdrop_path ? `${original}/${responseData.backdrop_path}` : unavailable}
+          alt=""
+          style={{ opacity: "0.15" }}
+        />
+      )}
+      {/*   {responseData && <img src={`${original}/${responseData.backdrop_path}`} alt="" style={{ opacity: "0.15" }} />} */}
       {responseData ? (
-        <div>
-          <img src={`${original}/${responseData.backdrop_path}`} alt="" width="500" style={{ opacity: "0.4" }} />
-          <p>{responseData.original_title || responseData.name}</p>
+        <div className="singleContentPage-inner">
+          <div className="content-top">
+            <img
+              src={responseData.poster_path ? `${img_500}/${responseData.poster_path}` : unavailable}
+              alt={responseData.name || responseData.title}
+              width="400"
+            />
+            <p style={{ color: "white" }}>{responseData.original_title || responseData.name}</p>
+          </div>
+
           {results && (
             <Button
               variant="contained"
